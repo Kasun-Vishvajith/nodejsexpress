881396b351760ad5c141579eea1c9bf6bdd57937
Ayathma <amaamanethmi@gmail.com>
2026-06-01 16:22:06 +0530

    added /about route and view

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const exphbs = require('express-handlebars');
 const index = require('./routes/index');
 const who = require('./routes/who');
 const contact = require('./routes/contact');
+const about = require('./routes/about');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+const hbsEngine = (typeof exphbs === 'function')
+  ? exphbs({ defaultLayout: 'main' })
+  : (exphbs && exphbs.engine)
+    ? exphbs.engine({ defaultLayout: 'main' })
+    : (exphbs && exphbs.create)
+      ? exphbs.create({ defaultLayout: 'main' }).engine
+      : null;
+if (!hbsEngine) {
+  throw new Error('Unsupported express-handlebars version');
+}
+app.engine('handlebars', hbsEngine);
 app.set('view engine', 'handlebars');
 app.set('port', config.port);
 
@@ -18,6 +29,7 @@ app.use('/', express.static('public'))
 app.use('/', index);
 app.use('/who', who);
 app.use('/contact', contact);
+app.use('/about', about);
 
 app.listen(config.port, () => {
   console.log(`Demo app is running on ${config.port}!`);
